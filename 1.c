5da2fe845662ff7476663cf9f4289910547b66aa
mansibansal2112 <mansibansal2112@gmail.com>
2018-02-20 14:21:15 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,25 @@
+
+	//concept of using one fork.
+
+
+	#include<stdio.h>
+
+	#include<unistd.h>
+
+	int main()
+
+	{
+
+             printf("\n\t\t\tOUTPUT\n\t);
+
+	     fork();
+
+	     printf("hello");
+
+	     return 0;
+
+	}
+
+
+
+				
